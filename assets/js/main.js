8153a5fb7f3cfce8a68c6a4a05d22a0e0a0568c9
Wellington dos Santos <w_santos1993@outlook.com>
2022-11-02 16:24:46 -0300

    Adicionando relogio dinamico no header do portifolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,6 +69,33 @@ const sr = ScrollReveal({
     reset: true
 });
 
+/*===== Horario dinâmico =====*/
+setInterval(function() {
+    const time = document.querySelector("#time");
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    if (hours > 12) {
+        hours = hours;
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    time.textContent = hours + ":" + minutes + ":" + seconds;
+})
+
+
 /*===== ano copyright dinâmico =====*/
 const ano = document.getElementById("ano");
 const anoAtual = new Date();
